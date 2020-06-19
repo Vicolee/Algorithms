a794094cb6431bf3e4cd9a43d759601ebf2e76cd
Vico Lee <vicoleezy@gmail.com>
2020-06-19 22:58:07 +0800

    First commit for linked list queue data structure

---
+++ Data-Structures/Queue/LinkedListQueue.java
@@ -0,0 +1,108 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedListQueue<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+
+    /**
+     * Having the Node class as static makes it more of a top-level
+     * class that is not bound to an instance of the outer class.
+     * Hence other classes can create instances of Node without
+     * creating any instance of LinkedListStack.
+     */
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public LinkedListQueue() {
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+    }
+
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        n++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Node<Item> oldfirst = first;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null; // Avoid loitering
+        return oldfirst.item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
+    }
+
+    public Iterator<Item> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private class LinkedIterator implements Iterator<Item> {
+        private Node<Item> current;
+
+        public LinkedIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException("Queue underflow");
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<String> queue = new LinkedListQueue<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                queue.enqueue(item);
+            else if (!queue.isEmpty())
+                StdOut.print(queue.dequeue() + " ");
+        }
+        StdOut.println("(" + queue.size() + " left on queue)");
+    }
+}
